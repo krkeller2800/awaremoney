6b7ebca7171e070426a1120a6b51d0b3728af958
Karl Keller <karl@komakode.com>
2026-01-23 11:16:43 -0500

    Initial Commit

---
+++ awaremoney/awaremoneyApp.swift
@@ -0,0 +1,17 @@
+//
+//  awaremoneyApp.swift
+//  awaremoney
+//
+//  Created by Karl Keller on 1/23/26.
+//
+
+import SwiftUI
+
+@main
+struct awaremoneyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
